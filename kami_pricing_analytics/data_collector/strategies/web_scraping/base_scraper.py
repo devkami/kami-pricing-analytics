5f4f71951174fdaaa4541f37062624c93db96ffb
Maicon de Menezes <maicondmenezes@gmail.com>
2024-03-20 13:00:06 -0300

    fix timeout error when try to read robots.txt and made general requests using httpx, set the right user agent, tests not implemented yet

--- kami_pricing_analytics/data_collector/strategies/web_scraping/base_scraper.py
+++ kami_pricing_analytics/data_collector/strategies/web_scraping/base_scraper.py
@@ -5,6 +5,7 @@ from urllib.parse import urlparse
 import httpx
 from pydantic import ConfigDict, Field
 from robotexclusionrulesparser import RobotExclusionRulesParser as Robots
+from contextlib import asynccontextmanager
 
 from kami_pricing_analytics.data_collector.strategies.base_strategy import (
     BaseStrategy,
@@ -16,9 +17,11 @@ from kami_pricing_analytics.data_collector.strategies.web_scraping.constants imp
 
 
 class BaseScraper(BaseStrategy):
+    user_agent: str = Field(default=DEFAULT_USER_AGENT)
+    http_client: httpx.AsyncClient = Field(default=None)
     base_url: str = Field(default='')
     robots_url: str = Field(default='')
-    crawl_delay: int = Field(default=DEFAULT_CRAWL_DELAY)
+    crawl_delay: int = Field(default=DEFAULT_CRAWL_DELAY)    
     logger_name: str = Field(default='pricing-scraper')
     logger: logging.Logger = Field(default=None)
 
@@ -31,19 +34,16 @@ class BaseScraper(BaseStrategy):
         self._crawl_delay_fetched = False
         self.set_logger(self.logger_name)
 
-    async def _ensure_crawl_delay(self):
-        if not self._crawl_delay_fetched:
-            self.crawl_delay = await self._get_crawl_delay_async()
-            self._crawl_delay_fetched = True
-
-    def _get_base_url(self) -> str:
-        parsed_url = urlparse(self.product_url)
-        return f'{parsed_url.scheme}://{parsed_url.netloc}'
-
+    @asynccontextmanager
+    async def get_http_client(self):
+        headers = {'User-Agent': self.user_agent}
+        async with httpx.AsyncClient(headers=headers) as client:
+            yield client
+    
     async def _get_crawl_delay_async(self) -> int:
-        rules = Robots()
+        rules = Robots()        
         try:
-            async with httpx.AsyncClient() as client:
+            async with self.get_http_client() as client:
                 response = await client.get(self.robots_url)
             rules.parse(response.text)
             user_agent = DEFAULT_USER_AGENT
@@ -53,16 +53,27 @@ class BaseScraper(BaseStrategy):
             self.logger.exception(f'Error getting crawl delay: {e}')
             return DEFAULT_CRAWL_DELAY
 
+    async def _ensure_crawl_delay(self):
+        if not self._crawl_delay_fetched:
+            self.crawl_delay = await self._get_crawl_delay_async()
+            self._crawl_delay_fetched = True
+
+    def _get_base_url(self) -> str:
+        parsed_url = urlparse(str(self.product_url))
+        return f'{parsed_url.scheme}://{parsed_url.netloc}'
+    
     def set_logger(self, logger_name: str):
         self.logger = logging.getLogger(logger_name)
 
     async def fetch_content(self) -> str:
         await self._ensure_crawl_delay()
-
-        async with httpx.AsyncClient() as client:
-            response = await client.get(self.product_url)
+        async with self.get_http_client() as client:
+            response = await client.get(str(self.product_url))
         return response.text
 
     @abstractmethod
     async def scrap_product(self) -> list:
         pass
+    
+    async def close_http_client(self):
+        await self.http_client.aclose()
