4b2abf48c8f61b93dccb54ede3aa4e98bca00e97
Maicon de Menezes <maicondmenezes@gmail.com>
2024-04-24 11:36:13 -0300

    Refactor Amazon scraper strategy for enhanced robustness and modularity
    This commit overhauls the Amazon scraper implementation in our `kami_pricing_analytics` project to enhance its robustness, reliability, and code modularity. Significant changes include the removal of redundant libraries and the simplification of the webdriver setup process, focusing on the essential components needed for efficient scraping.
    
    Key Changes:
    - Removed unused imports such as `asyncio`, `random`, and `time` from the Amazon scraper, streamlining the dependencies and focusing on core functionality.
    - Decoupled the webdriver setup from the scraping logic, removing the custom driver setup method `_setup_driver` which included unnecessary user-agent rotation and stealth options that were complicating the scraper setup without significant benefits.
    - Introduced a new structure for handling product data fetching, encapsulating functionality into smaller, more manageable methods like `get_marketplace_id`, `get_brand`, `get_description`, `get_price`, `get_seller_id`, and `get_seller_name`. Each of these methods is designed to handle specific aspects of the scraping process, making the code easier to maintain and debug.
    - Replaced direct attribute fetching with methods that handle exceptions and ensure data integrity, preventing common errors such as `StaleElementReferenceException` by implementing robust exception handling and retries within these methods.
    - Enhanced logging to provide more detailed insights into the execution flow and errors, which will help in quicker troubleshooting and ensures that the scraper's operation is transparent and predictable.
    
    Overall, this refactor makes the Amazon scraper more aligned with modern Python practices and prepares the foundation for future extensions, such as adding more sophisticated error handling or supporting additional product types.
    
    By restructuring our approach to the scraper's architecture, we're aiming to increase maintainability and scalability, which is crucial as we expand our data collection strategies to cover more marketplaces and product categories.

--- kami_pricing_analytics/data_collector/strategies/web_scraping/amazon.py
+++ kami_pricing_analytics/data_collector/strategies/web_scraping/amazon.py
@@ -1,28 +1,12 @@
-import asyncio
-import logging
-import random
-import time
-from concurrent.futures import ThreadPoolExecutor
 from urllib.parse import parse_qs, urlparse
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium_stealth import stealth
-from webdriver_manager.chrome import ChromeDriverManager
 
 from kami_pricing_analytics.data_collector.strategies.web_scraping.base_scraper import (
     BaseScraper,
 )
-from kami_pricing_analytics.data_collector.strategies.web_scraping.constants import (
-    USER_AGENTS,
-)
-
-amazon_scraper_logger = logging.getLogger('amazon-scraper')
 
 
 class AmazonScraperException(Exception):
@@ -30,66 +14,31 @@ class AmazonScraperException(Exception):
 
 
 class AmazonScraper(BaseScraper):
-    def _setup_driver(self):
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
+    def __init__(self, **data):
+        super().__init__(**data, logger_name='amazon-scraper')
 
-        # User-Agent Rotation
-        user_agent = random.choice(USER_AGENTS)
-        print(f'Using User-Agent: {user_agent}')
-        options.add_argument(f'user-agent={user_agent}')
-
-        driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()), options=options
-        )
-        stealth(
-            driver,
-            languages=['en-US', 'en'],
-            vendor='Google Inc.',
-            platform='Win32',
-            webgl_vendor='Intel Inc.',
-            renderer='Intel Iris OpenGL Engine',
-            fix_hairline=True,
-        )
-        return driver
-
-    async def get_driver(self) -> WebDriver:
-        with ThreadPoolExecutor() as executor:
-            loop = asyncio.get_running_loop()
-            future = loop.run_in_executor(executor, self._setup_driver)
-            driver = await future
-            return driver
-
-    async def scrap_product(self):
-        driver = await self.get_driver()
-        driver.get(str(self.product_url))
-        request_interval = random.uniform(1, 3)
-        amazon_scraper_logger.info(
-            f'Waiting {request_interval} seconds before scraping product'
-        )
-        time.sleep(request_interval)
-
+    """ async def scrap_product(self):
+        await self.set_webdriver()
+        self.webdriver.get(str(self.product_url))
         sellers_list = []
         try:
-            asin = driver.find_element(
+            asin = self.webdriver.find_element(
                 By.XPATH,
                 '//th[contains(text(), "ASIN")]/following-sibling::td',
             ).text.strip()
-            description = driver.find_element(By.ID, 'title').text.strip()
-            brand = driver.find_element(
+            description = self.webdriver.find_element(By.ID, 'title').text.strip()
+            brand = self.webdriver.find_element(
                 By.XPATH,
                 '//th[contains(text(), "Fabricante")]/following-sibling::td',
             ).text.strip()
-            clickable_element = driver.find_element(
+            clickable_element = self.webdriver.find_element(
                 By.CSS_SELECTOR, 'a.a-touch-link.a-box.olp-touch-link'
             )
             clickable_element.click()
-            WebDriverWait(driver, 10).until(
+            WebDriverWait(self.webdriver, 10).until(
                 EC.visibility_of_element_located((By.ID, 'aod-offer-list'))
             )
-            offers = driver.find_elements(
+            offers = self.webdriver.find_elements(
                 By.CSS_SELECTOR, '#aod-offer-list #aod-offer'
             )
 
@@ -120,8 +69,138 @@ class AmazonScraper(BaseScraper):
             raise AmazonScraperException(f'Error when scraping product: {e}')
 
         finally:
-            driver.quit()
+            self.webdriver.quit()
             return sellers_list
+    """
+
+    async def get_marketplace_id(self):
+        marketplace_id = ''
+        try:
+            marketplace_id = self.webdriver.find_element(
+                By.XPATH,
+                '//th[contains(text(), "ASIN")]/following-sibling::td',
+            ).text.strip()
+        except Exception as e:
+            raise AmazonScraperException(f'Error while getting ASIN: {e}')
+
+        return marketplace_id
+
+    async def get_brand(self):
+        brand = ''
+        try:
+            brand = self.webdriver.find_element(
+                By.XPATH,
+                '//th[contains(text(), "Fabricante")]/following-sibling::td',
+            ).text.strip()
+        except Exception as e:
+            raise AmazonScraperException(
+                f'Error while getting product brand: {e}'
+            )
+
+        return brand
+
+    async def get_description(self):
+        description = ''
+        try:
+            description = self.webdriver.find_element(
+                By.ID, 'title'
+            ).text.strip()
+        except Exception as e:
+            raise AmazonScraperException(
+                f'Error while getting product description: {e}'
+            )
+
+        return description
+
+    async def get_price(self, seller):
+        price = ''
+        try:
+            price = seller.find_element(
+                By.CSS_SELECTOR, 'span.a-offscreen'
+            ).get_attribute('innerHTML')
+        except Exception as e:
+            raise AmazonScraperException(
+                f'Error while getting product price: {e}'
+            )
+
+        return price
+
+    async def get_seller_id(self, seller):
+        seller_id = ''
+        try:
+            seller_link = seller.find_element(
+                By.CSS_SELECTOR, 'a.a-size-small.a-link-normal'
+            ).get_attribute('href')
+            seller_id = parse_qs(urlparse(seller_link).query).get(
+                'seller', [None]
+            )[0]
+        except Exception as e:
+            raise AmazonScraperException(f'Error while getting seller id: {e}')
+
+        return seller_id
+
+    async def get_seller_name(self, seller):
+        seller_name = ''
+        try:
+            seller_name = seller.find_element(
+                By.CSS_SELECTOR, 'a.a-size-small.a-link-normal'
+            ).get_attribute('innerHTML')
+        except Exception as e:
+            raise AmazonScraperException(
+                f'Error while getting seller name: {e}'
+            )
+
+        return seller_name
+
+    async def get_sellers_list(self):
+        sellers = []
+        try:
+            self.webdriver.get(str(self.product_url))
+            clickable_element = self.webdriver.find_element(
+                By.XPATH,
+                '//div[@class="a-section a-spacing-none daodi-content"]//a[@class="a-link-normal"]',
+            )
+            clickable_element.click()
+            WebDriverWait(self.webdriver, 10).until(
+                EC.visibility_of_element_located((By.ID, 'aod-offer-list'))
+            )
+            sellers_offers = self.webdriver.find_elements(
+                By.CSS_SELECTOR, '#aod-offer-list #aod-offer'
+            )
+            for seller_offer in sellers_offers:
+                seller = {
+                    'price': '',
+                    'seller_id': '',
+                    'seller_name': '',
+                }
+                seller['price'] = await self.get_price(seller_offer)
+                seller['seller_id'] = await self.get_seller_id(seller_offer)
+                seller['seller_name'] = await self.get_seller_name(
+                    seller_offer
+                )
+                sellers.append(seller)
+        except Exception as e:
+            raise AmazonScraperException(
+                f'Error while getting sellers list: {e}'
+            )
+
+        return sellers
+
+    async def get_seller_info(self, seller):
+        try:
+            self.webdriver.get(str(self.product_url))
+            seller['marketplace_id'] = await self.get_marketplace_id()
+            seller['brand'] = await self.get_brand()
+            seller['description'] = await self.get_description()
+
+        except AmazonScraperException as e:
+            self.logger.error(f'Error while getting seller info details: {e}')
+        except Exception as e:
+            self.logger.error(
+                f'Unexpected error while getting seller info: {e}'
+            )
+
+        return seller
 
     async def execute(self) -> dict:
         return {'result': await self.scrap_product()}
