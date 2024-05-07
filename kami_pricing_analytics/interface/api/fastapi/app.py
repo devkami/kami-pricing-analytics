e8179f94ef9edbf281687bd3eb13fa1ee19cdcc7
Maicon de Menezes <maicondmenezes@gmail.com>
2024-05-07 14:35:04 -0300

    --- Issue: Fix payload structure and update API documentation for pricing research endpoint
    Description:
    Refactored the payload structure for the pricing research endpoint to accept a subset of fields from the `PricingResearchRequest` model. Introduced a new `PricingResearchPayload` model containing the required fields for the request payload. Modified the endpoint function to instantiate the `PricingResearchRequest` object using the received payload, leveraging the `**` syntax for keyword argument unpacking. Updated the API documentation for the `research` endpoint to provide a detailed description of the payload structure and supported options.
    
    Changes:
    - Moved `pricing_research_request.py` from `kami_pricing_analytics/interface/api` to `kami_pricing_analytics/schemas/`.
    - Added `PricingResearchPayload` model to define the payload structure for the `research` endpoint.
    - Updated the `research` endpoint function to accept `PricingResearchPayload` as input and instantiate `PricingResearchRequest` with the payload data.
    - Updated the API documentation with a detailed description of the payload structure, supported marketplaces, strategies, and storage options.
    
    Version: 0.6.1
    ---

--- kami_pricing_analytics/interface/api/fastapi/app.py
+++ kami_pricing_analytics/interface/api/fastapi/app.py
@@ -1,28 +1,54 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from fastapi import APIRouter, FastAPI, HTTPException, status
 from fastapi.responses import JSONResponse
-
-from kami_pricing_analytics.interface.api.pricing_research_request import (
+from pydantic import BaseModel, Field
+from kami_pricing_analytics.schemas.pricing_research_request import (
     PricingResearchRequest,
 )
+from kami_pricing_analytics.schemas.options import StrategyOptions
 
 research_app = FastAPI(
     title='KAMI-Pricing Analytics API',
     description="API to conduct pricing research over a product's URL.",
-    version='0.6.0',
+    version='0.6.1',
 )
 api_router = APIRouter()
 
+
+class PricingResearchPayload(BaseModel):
+    url: Optional[str] = Field(default=None)
+    marketplace: Optional[str] = Field(default=None)
+    marketplace_id: Optional[str] = Field(default=None)
+    strategy_option: int = Field(default=StrategyOptions.WEB_SCRAPING.value)
+    store_result: bool = Field(default=False)
 
 @research_app.post(
     '/research',
     response_model=Dict[str, List[Dict[str, Any]]],
     status_code=status.HTTP_200_OK,
+    summary='Conduct a pricing research over a product.',
+    description="""
+    Conduct a pricing research over a product for a given URL on marketplace. or The Market Place Name and the Marketplace ID for that product. The results is a list of sellers and their prices for this product. It's also allows to store the results in a database. and choose the strategy to be used to collect the data. 
+    
+    Payload
+    - `url`: URL of the product on the marketplace.
+    - `marketplace`: Name of the marketplace.
+      Marketplaces supported: 
+        - beleza_na_web: Beleza na Web
+        - amazon: Amazon 
+        - mercado_livre: Mercado Livre 
+    - `marketplace_id`: ID of the product on the marketplace.
+    - `strategy`: Strategy to be used to collect the data.
+      Strategies supported:
+        - 0: Web Scraping
+    - `store_result`: Store the results in a database.
+    """,
 )
-async def research(request: PricingResearchRequest):
+async def research(payload: PricingResearchPayload):
     try:
 
+        request = PricingResearchRequest(**payload.model_dump())
         sellers = await request.post()
         return {'result': sellers}
 
