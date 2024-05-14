50328780650972ffc1a15343f0c1d23ecd93ff32
Maicon de Menezes <maicondmenezes@gmail.com>
2024-05-14 19:30:27 -0300

    feat(services): Enhance PricingService for robust research management and error handling
    - Introduced `PricingServiceException` for better error handling.
    - Refactored `PricingService` class to include more detailed method docstrings and validations.
    - Added methods to conduct, store, and retrieve research with comprehensive error management.
    - Enhanced strategy and storage setup with proper exception handling.
    - Improved service operations to ensure robustness and reliability in handling research data.
    
    BREAKING CHANGE: Changed the structure and flow of `PricingService` methods, renamed imports for consistency.

--- kami_pricing_analytics/services/pricing_service.py
+++ kami_pricing_analytics/services/pricing_service.py
@@ -1,26 +1,40 @@
-import configparser
 import json
-import os
 
 from pydantic import BaseModel, ConfigDict, Field, model_validator
 
-from kami_pricing_analytics.data_collector.collector import StrategyFactory
-from kami_pricing_analytics.data_collector.strategies.base_strategy import (
-    BaseStrategy,
+from kami_pricing_analytics.data_collector import (
+    BaseCollector,
+    CollectorFactory,
+    CollectorOptions,
 )
-from kami_pricing_analytics.data_storage.base_storage import BaseStorage
-from kami_pricing_analytics.data_storage.storage_factory import (
-    StorageFactory,
-    StorageModeOptions,
-)
-from kami_pricing_analytics.schemas.options import StrategyOptions
-from kami_pricing_analytics.schemas.pricing_research import PricingResearch
+from kami_pricing_analytics.data_storage import BaseStorage, StorageFactory
+from kami_pricing_analytics.schemas import PricingResearch
+
+
+class PricingServiceException(Exception):
+    """
+    Custom exception class for PricingService-related errors.
+    """
+
+    pass
 
 
 class PricingService(BaseModel):
+    """
+    Service to manage pricing data research and storage operations, integrating
+    strategies for data collection and storage mechanisms.
 
-    strategy_option: int = Field(default=StrategyOptions.WEB_SCRAPING.value)
-    strategy: BaseStrategy = Field(default=None)
+    Attributes:
+        collector_option (int): Identifier for the strategy to use for data collection.
+        strategy (BaseCollector): The strategy instance for data collection.
+        store_result (bool): Flag to determine if results should be stored.
+        storage_mode (int): Identifier for the storage mode to use.
+        storage (BaseStorage): The storage instance for data management.
+        research (PricingResearch): The research data to process.
+    """
+
+    collector_option: int = Field(default=CollectorOptions.WEB_SCRAPING.value)
+    strategy: BaseCollector = Field(default=None)
 
     store_result: bool = Field(default=False)
     storage_mode: int = Field(default=0)
@@ -36,34 +50,147 @@ class PricingService(BaseModel):
     )
 
     @model_validator(mode='after')
-    def set_strategy(self):
-        self.strategy = StrategyFactory.get_strategy(
-            strategy_option=self.strategy_option,
-            product_url=str(self.research.url),
-        )
+    def set_strategy(self) -> 'PricingService':
+        """
+        Sets the strategy based on the current strategy option and research URL.
+
+        Raises:
+            PricingServiceException: If there is an error in setting up the strategy.
+
+        Returns:
+            PricingService: The instance of this service with an updated strategy.
+        """
+        try:
+            self.strategy = CollectorFactory.get_strategy(
+                collector_option=self.collector_option,
+                product_url=str(self.research.url),
+            )
+        except ValueError as e:
+            raise PricingServiceException(
+                f'Value Error while setting strategy: {e}'
+            )
+        except Exception as e:
+            raise PricingServiceException(
+                f'Unexpected error while setting strategy: {e}'
+            )
+
         return self
 
     @model_validator(mode='after')
-    def set_storage(self):
+    def set_storage(self) -> 'PricingService':
+        """
+        Sets the storage mode based on the current storage mode setting and store_result flag.
 
-        if self.store_result:
-            self.storage = StorageFactory.get_mode(mode=self.storage_mode)
+        Raises:
+            PricingServiceException: If there is an error in setting up the storage.
+
+        Returns:
+            PricingService: The instance of this service with an updated storage setting.
+        """
+
+        try:
+            if self.store_result:
+                self.storage = StorageFactory.get_mode(mode=self.storage_mode)
+        except ValueError as e:
+            raise PricingServiceException(
+                f'Value Error while setting storage: {e}'
+            )
+        except Exception as e:
+            raise PricingServiceException(
+                f'Unexpected error while setting storage: {e}'
+            )
+
         return self
 
-    async def conduct_research(self):
-        """Execute the assigned strategy to conduct research and store the results if necessary."""
+    async def conduct_research(self) -> bool:
+        """
+        Conducts the research using the assigned strategy and updates the research data.
 
-        result = await self.strategy.execute()
+        Raises:
+            PricingServiceException: If there is an error during the research process.
 
-        self.research.update_research_data(result)
+        Returns:
+            bool: True if the research was successful, False otherwise.
+        """
 
-    async def store_research(self):
-        if self.store_result:
-            research_data = self.research.model_dump_json(
-                exclude={'model_config'}
+        is_conducted = False
+
+        try:
+            result = await self.strategy.execute()
+            self.research.update_research_data(result)
+            is_conducted = True
+        except ValueError as e:
+            raise PricingServiceException(
+                f'Value Error while conducting research: {e}'
             )
-            research_data = json.loads(research_data)
-            research_data['strategy'] = StrategyOptions(
-                self.strategy_option
-            ).name
-            await self.storage.save(research_data)
+        except Exception as e:
+            raise PricingServiceException(
+                f'Unexpected error while conducting research: {e}'
+            )
+
+        return is_conducted
+
+    async def store_research(self) -> bool:
+        """
+        Stores the research data using the configured storage mode if storage is enabled.
+
+        Raises:
+            PricingServiceException: If there is an error during the storage process.
+
+        Returns:
+            bool: True if the storage was successful, False otherwise.
+        """
+        is_result_stored = False
+
+        try:
+            if self.store_result:
+                research_data = self.research.model_dump_json(
+                    exclude={'model_config'}
+                )
+                research_data = json.loads(research_data)
+                research_data['strategy'] = CollectorOptions(
+                    self.collector_option
+                ).name
+                await self.storage.save(research_data)
+                is_result_stored = True
+        except ValueError as e:
+            raise PricingServiceException(
+                f'Value Error while storing research: {e}'
+            )
+        except Exception as e:
+            raise PricingServiceException(
+                f'Unexpected error while storing research: {e}'
+            )
+
+        return is_result_stored
+
+    async def retrieve_research(self) -> bool:
+        """
+        Retrieves the research data based on specified criteria and updates the research attribute.
+
+        Raises:
+            PricingServiceException: If there is an error during the retrieval process.
+
+        Returns:
+            bool: True if the retrieval was successful, False otherwise.
+        """
+        is_research_retrieved = False
+        try:
+
+            criteria = {
+                'marketplace': self.research.marketplace,
+                'marketplace_id': self.research.marketplace_id,
+            }
+            results = await self.storage.retrieve(criteria)
+            if results:
+                research_data = results[0]
+                self.research = PricingResearch(**research_data)
+                is_research_retrieved = True
+        except ValueError as e:
+            raise ValueError(f'Value Error while retrieving research: {e}')
+        except Exception as e:
+            raise PricingServiceException(
+                f'Unexpected error while retrieving research: {e}'
+            )
+
+        return is_research_retrieved
