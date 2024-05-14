b0ff03ddca18c43ba2ec4275af26b5b355f91c3c
Maicon de Menezes <maicondmenezes@gmail.com>
2024-05-14 19:26:11 -0300

    refactor(data_storage): restructure and improve data storage module
    - Rename and update imports for storage classes and settings
    - Enhance BaseStorage class with additional attributes and methods
    - Improve various storage classes with detailed docstrings and error handling
    - Update SQLServerStorage, MySQLStorage, PLSQLStorage, PostgreSQLStorage, and SQLiteStorage with proper configuration and documentation
    - Refactor and document common functionality in DatabaseStorage and StorageFactory
    
    docs: add module-level docstrings and detailed descriptions for methods
    
    - Add comprehensive docstrings to base_storage.py, relational models, and all storage classes
    - Update module-level docstrings for data_storage strategies
    
    feat(data_storage): add additional error handling and utility methods
    
    - Add utility methods for constructing database URLs in settings classes
    - Implement detailed error handling in storage classes to provide more context on failures

--- kami_pricing_analytics/data_storage/modes/database/relational/mysql.py
+++ kami_pricing_analytics/data_storage/modes/database/relational/mysql.py
@@ -1,20 +1,28 @@
 from pydantic import Field
 
-from kami_pricing_analytics.data_storage.modes.database.relational.storage import (
-    DatabaseSettings,
-    DatabaseStorage,
-)
+from .storage import DatabaseSettings, DatabaseStorage
 
 
 class MySQLSettings(DatabaseSettings):
+    """
+    Configuration settings specific to MySQL database.
+
+    Attributes:
+        db_driver (str): Database driver, defaulting to 'mysql+aiomysql'.
+        db_port (int): Database port, default is 3306.
+    """
+
     db_driver: str = 'mysql+aiomysql'
     db_port: int = Field(default=3306)
 
     @property
-    def db_url(self):
+    def db_url(self) -> str:
+        """Constructs the database URL based on the MySQL settings."""
         return f'{self.db_driver}://{self.db_user}:{self.db_password.get_secret_value()}@{self.db_host}:{self.db_port}/{self.db_name}'
 
 
 class MySQLStorage(DatabaseStorage):
+    """Storage class for MySQL database using the specified settings."""
+
     def __init__(self, settings: MySQLSettings):
         super().__init__(settings)
--- kami_pricing_analytics/data_storage/storage_factory.py
+++ kami_pricing_analytics/data_storage/storage_factory.py
@@ -1,39 +1,19 @@
-from enum import Enum
 from typing import Dict, Type
 
-from kami_pricing_analytics.data_storage.base_storage import BaseStorage
-from kami_pricing_analytics.data_storage.modes.database.relational.mssql import (
+from kami_pricing_analytics.data_storage import BaseStorage, StorageModeOptions
+from kami_pricing_analytics.data_storage.modes.database.relational import (
+    DatabaseSettings,
+    MySQLSettings,
+    MySQLStorage,
+    PLSQLSettings,
+    PLSQLStorage,
+    PostgreSQLSettings,
+    PostgreSQLStorage,
+    SQLiteSettings,
+    SQLiteStorage,
     SQLServerSettings,
     SQLServerStorage,
 )
-from kami_pricing_analytics.data_storage.modes.database.relational.mysql import (
-    MySQLSettings,
-    MySQLStorage,
-)
-from kami_pricing_analytics.data_storage.modes.database.relational.plsql import (
-    PLSQLSettings,
-    PLSQLStorage,
-)
-from kami_pricing_analytics.data_storage.modes.database.relational.postgresql import (
-    PostgreSQLSettings,
-    PostgreSQLStorage,
-)
-from kami_pricing_analytics.data_storage.modes.database.relational.settings import (
-    DatabaseSettings,
-)
-from kami_pricing_analytics.data_storage.modes.database.relational.sqlite import (
-    SQLiteSettings,
-    SQLiteStorage,
-)
-
-
-class StorageModeOptions(Enum):
-    SQLITE = 0
-    POSTGRESQL = 1
-    MYSQL = 2
-    SQLSERVER = 3
-    PLSQL = 4
-
 
 storage_options = [
     f'{option.value} - {option.name}' for option in StorageModeOptions
@@ -41,6 +21,14 @@ storage_options = [
 
 
 class StorageFactory:
+    """
+    Factory class for managing storage instances across different database technologies.
+
+    Attributes:
+        storage_mapping (Dict[StorageModeOptions, Type[BaseStorage]]): Maps storage modes to corresponding storage classes.
+        settings_mapping (Dict[StorageModeOptions, Type[DatabaseSettings]]): Maps storage modes to corresponding settings classes.
+    """
+
     storage_mapping: Dict[StorageModeOptions, Type[BaseStorage]] = {}
     settings_mapping: Dict[StorageModeOptions, Type[DatabaseSettings]] = {}
 
@@ -50,37 +38,124 @@ class StorageFactory:
         mode: StorageModeOptions,
         storage: Type[BaseStorage],
         settings: Type[DatabaseSettings],
-    ):
-        cls.storage_mapping[mode] = storage
-        cls.settings_mapping[mode] = settings
+    ) -> bool:
+        """
+        Registers a storage mode with its corresponding storage and settings classes.
+
+        Args:
+            mode (StorageModeOptions): The storage mode identifier.
+            storage (Type[BaseStorage]): The storage class.
+            settings (Type[DatabaseSettings]): The settings class.
+
+        Returns:
+            bool: True if the storage mode was successfully registered, False otherwise.
+
+        Raises:
+            ValueError: If an error occurs while registering the storage mode.
+        """
+
+        is_registered = False
+        try:
+            cls.storage_mapping[mode] = storage
+            cls.settings_mapping[mode] = settings
+            is_registered = True
+        except Exception as e:
+            raise ValueError(f'Error while registering storage mode: {e}')
+
+        return is_registered
 
     @classmethod
     def get_mode(cls, mode: int) -> Type[BaseStorage]:
-        storage_mode_option = StorageModeOptions(mode)
-        if (
-            storage_mode_option not in cls.storage_mapping
-            and storage_mode_option not in cls.settings_mapping
-        ):
-            raise ValueError(
-                f'Unsupported STORAGE_MODE: {mode}. Available options: {storage_options}'
-            )
-        settings_class = cls.settings_mapping.get(storage_mode_option)
-        settings = settings_class()
-        storage_class = cls.storage_mapping.get(storage_mode_option)
-        storage_mode = storage_class(settings=settings)
+        """
+        Retrieves a storage instance configured with appropriate settings for the specified mode.
+
+        Args:
+            mode (int): The storage mode identifier.
+
+        Returns:
+            Type[BaseStorage]: An instance of the corresponding storage class.
+        """
+        storage_mode = Type[BaseStorage]
+        try:
+            storage_mode_option = StorageModeOptions(mode)
+            if (
+                storage_mode_option not in cls.storage_mapping
+                and storage_mode_option not in cls.settings_mapping
+            ):
+                raise ValueError(
+                    f'Unsupported STORAGE_MODE: {mode}. Available options: {storage_options}'
+                )
+            settings_class = cls.settings_mapping.get(storage_mode_option)
+            settings = settings_class()
+            storage_class = cls.storage_mapping.get(storage_mode_option)
+            storage_mode = storage_class(settings=settings)
+        except Exception as e:
+            raise ValueError(f'Error while retrieving storage mode: {e}')
+
         return storage_mode
 
 
 class DatabaseSettingsFactory:
+    """
+    Factory class for managing database settings instances specific to storage modes.
+
+    Attributes:
+        settings_mapping (Dict[StorageModeOptions, Type[DatabaseSettings]]): Maps storage modes to corresponding settings classes.
+    """
+
     settings_mapping: Dict[StorageModeOptions, Type[DatabaseSettings]] = {}
 
     @classmethod
-    def register_settings(cls, mode: int, settings: Type[DatabaseSettings]):
-        cls.settings_mapping[StorageModeOptions(mode)] = settings
+    def register_settings(
+        cls, mode: int, settings: Type[DatabaseSettings]
+    ) -> bool:
+        """
+        Registers database settings for a specific storage mode.
+
+        Args:
+            mode (int): The database mode identifier.
+            settings (Type[DatabaseSettings]): The settings class.
+
+        Returns:
+            bool: True if the database settings were successfully registered, False otherwise.
+
+        Raises:
+            ValueError: If an error occurs while registering the database settings.
+        """
+
+        is_registered = False
+        if StorageModeOptions(mode) not in StorageModeOptions:
+            raise ValueError(
+                f'Unsupported Database Settings: {mode}. Available options: {storage_options}'
+            )
+
+        try:
+            cls.settings_mapping[StorageModeOptions(mode)] = settings
+            is_registered = True
+        except Exception as e:
+            raise ValueError(f'Error while registering database settings: {e}')
+
+        return is_registered
 
     @classmethod
     def get_settings(cls, mode: int) -> Type[DatabaseSettings]:
-        settings = cls.settings_mapping.get(StorageModeOptions(mode))
+        """
+        Retrieves the settings instance for the specified database mode.
+
+        Args:
+            mode (int): The database mode identifier.
+
+        Returns:
+            Type[DatabaseSettings]: An instance of the corresponding settings class.
+
+        Raises:
+            ValueError: If an error occurs while retrieving the database settings.
+        """
+        try:
+            settings = cls.settings_mapping.get(StorageModeOptions(mode))
+        except Exception as e:
+            raise ValueError(f'Error while retrieving database settings: {e}')
+
         if settings is None:
             raise ValueError(
                 f'Unsupported Database Settings: {mode}. Available options: {storage_options}'
