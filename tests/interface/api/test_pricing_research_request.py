0a7e411abf3a2a3dc05fec0d8f08c040fd67cbdc
Maicon de Menezes <maicondmenezes@gmail.com>
2024-05-14 19:36:13 -0300

    test: update tests for PricingResearchRequest to align with new CollectorOptions and exception handling
    - Replace StrategyOptions with CollectorOptions in tests
    - Update tests to use PricingResearchRequestException for error handling
    - Ensure proper validation of strategy option and research parameters
    - Add tests for new scenarios including expired research data handling

--- tests/interface/api/test_pricing_research_request.py
+++ tests/interface/api/test_pricing_research_request.py
@@ -1,13 +1,15 @@
 import asyncio
 import unittest
+from datetime import datetime, timedelta, timezone
 from unittest.mock import AsyncMock, patch
 
-from kami_pricing_analytics.schemas.options import StrategyOptions
-from kami_pricing_analytics.schemas.pricing_research import PricingResearch
-from kami_pricing_analytics.schemas.pricing_research_request import (
+from kami_pricing_analytics.data_collector import CollectorOptions
+from kami_pricing_analytics.interface.api import (
     PricingResearchRequest,
+    PricingResearchRequestException,
 )
-from kami_pricing_analytics.services.pricing_service import PricingService
+from kami_pricing_analytics.schemas import PricingResearch
+from kami_pricing_analytics.services import PricingService
 
 
 class TestPricingResearchRequest(unittest.IsolatedAsyncioTestCase):
@@ -16,7 +18,7 @@ class TestPricingResearchRequest(unittest.IsolatedAsyncioTestCase):
             'url': 'https://www.amazon.com.br/dp/B07GYX8QRJ',
             'marketplace': 'AMAZON',
             'marketplace_id': 'B07GYX8QRJ',
-            'strategy_option': StrategyOptions.WEB_SCRAPING.value,
+            'collector_option': CollectorOptions.WEB_SCRAPING.value,
             'store_result': True,
         }
         self.request = PricingResearchRequest(**self.valid_data)
@@ -30,7 +32,7 @@ class TestPricingResearchRequest(unittest.IsolatedAsyncioTestCase):
             self.request.marketplace_id, self.valid_data['marketplace_id']
         )
         self.assertEqual(
-            self.request.strategy_option, self.valid_data['strategy_option']
+            self.request.collector_option, self.valid_data['collector_option']
         )
         self.assertEqual(
             self.request.store_result, self.valid_data['store_result']
@@ -39,14 +41,14 @@ class TestPricingResearchRequest(unittest.IsolatedAsyncioTestCase):
         self.assertIsInstance(self.request.service.research, PricingResearch)
 
     def test_wrong_strategy_option_raises_error(self):
-        self.request.strategy_option = 99
-        with self.assertRaises(ValueError):
+        self.request.collector_option = 99
+        with self.assertRaises(PricingResearchRequestException):
             self.request.validate_strategy_option()
 
     def test_missing_url_and_marketplace_info_raises_error(self):
         self.request.url = None
         self.request.marketplace = None
-        with self.assertRaises(ValueError):
+        with self.assertRaises(PricingResearchRequestException):
             self.request.validate_research()
 
     @patch(
@@ -77,12 +79,21 @@ class TestPricingResearchRequest(unittest.IsolatedAsyncioTestCase):
     async def test_when_store_result_post_should_create_store_research_task(
         self, mock_conduct_research, mock_store_research, mock_create_task
     ):
-        mock_store_research.return_value = await asyncio.sleep(0)
+        async def mock_create_task_func(coro):
+            await coro
+
+        mock_create_task.side_effect = mock_create_task_func
         self.request.store_result = True
 
         await self.request.post()
 
         mock_create_task.assert_called_once()
+        called_coro = mock_create_task.call_args[0][0]
+
+        self.assertTrue(
+            asyncio.iscoroutine(called_coro),
+            'Expected a coroutine to be scheduled for create_task.',
+        )
         mock_conduct_research.assert_awaited()
 
     @patch(
@@ -102,6 +113,43 @@ class TestPricingResearchRequest(unittest.IsolatedAsyncioTestCase):
         result = await self.request.post()
 
         self.assertEqual(result, self.request.service.research.sellers)
+
+    @patch(
+        'kami_pricing_analytics.services.pricing_service.PricingService.retrieve_research'
+    )
+    async def test_get_success_not_expired(self, mock_retrieve_research):
+        self.request.service.research.sellers = ['seller1', 'seller2']
+        self.request.service.research.conducted_at = datetime.now(
+            tz=timezone.utc
+        )
+
+        self.assertFalse(self.request.service.research.expired)
+        self.assertEqual(
+            self.request.service.research.sellers, ['seller1', 'seller2']
+        )
+
+    @patch(
+        'kami_pricing_analytics.services.pricing_service.PricingService.retrieve_research'
+    )
+    @patch(
+        'kami_pricing_analytics.interface.api.pricing_research_request.PricingResearchRequest.post'
+    )
+    async def test_get_expired_data_calls_post(
+        self, mock_post, mock_retrieve_research
+    ):
+        self.request.service.research.sellers = ['seller1', 'seller2']
+        self.request.service.research.conducted_at = datetime.now(
+            tz=timezone.utc
+        ) - timedelta(seconds=3600)
+
+        await self.request.get()
+
+        mock_post.assert_awaited()
+
+        self.assertTrue(self.request.service.research.expired)
+        self.assertEqual(
+            self.request.service.research.sellers, ['seller1', 'seller2']
+        )
 
 
 if __name__ == '__main__':
